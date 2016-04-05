b73e37af79501dffedc391793c09b3c3adedbc1e
akhr <akhashr@gmail.com>
2016-04-05 13:33:13 -0700

    Adding alogorithm to merge two sorted arrays into one sorted array.

---
+++ Algorithms_Java_Implementation/src/com/ak/algorithms/MergeTwoSortedArrays.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package com.ak.algorithms;
+
+import com.ak.algorithms.sorting.practice.QuickSorter;
+import com.ak.utils.Utils;
+
+/**
+ * @author Akhash Ramamurthy
+ *
+ * Apr 5, 2016 11:41:13 AM
+ * MergeTwoSortedArrays.java
+ */
+public class MergeTwoSortedArrays {
+
+
+	public static void main(String[] args){
+		int[] input_1 = Utils.intGenerator(10, -80, 2000);
+		int[] input_2 = Utils.intGenerator(6, 70, 999);
+		
+		QuickSorter quickSorter = new QuickSorter();
+		input_1 = quickSorter.sortArray(input_1);
+		input_2 = quickSorter.sortArray(input_2);
+		
+		int[] resultArr = merge(input_1, input_2);
+		Utils.printArray("resultArr : Sorted ", resultArr);
+	}
+
+	public static int[] merge(int[] a, int[] b){
+		int[] result = new int[a.length + b.length];
+		int aIndex = 0, bIndex = 0, resIndex = 0;
+
+		while(aIndex < a.length && bIndex < b.length){
+			if(a[aIndex] == b[bIndex]){
+				result[resIndex++] = a[aIndex++];
+				result[resIndex++] = b[bIndex++];
+			}
+			if(a[aIndex] < b[bIndex]){
+				result[resIndex] = a[aIndex];
+				aIndex++;
+				resIndex++;
+			}
+			if(a[aIndex] > b[bIndex]){
+				result[resIndex] = b[bIndex];
+				bIndex++;
+				resIndex++;
+			}
+		}
+
+		while(aIndex < a.length){
+			result[resIndex++] = a[aIndex++];
+		}
+
+		while(bIndex < b.length){
+			result[resIndex++] = b[bIndex++];
+		}
+
+		return result;
+	}
+
+}
