e75d34749733691f88cd7bb3d78e34585d8e084b
ramamurthy <akhashr@gmail.com>
2016-12-28 22:18:47 -0800

    Adding Fibonacci implementation using traditional recursive method, bottom up DP and finally using 3 variables. Traditional recursive method doesnt work for n >= 100.. Surprising...

--- Algorithms_Java_Implementation/src/com/akh/algorithms/dynamicProgramming/Fibbonaci.java
+++ Algorithms_Java_Implementation/src/com/akh/algorithms/dynamicProgramming/Fibbonaci.java
@@ -6,22 +6,23 @@ package com.akh.algorithms.dynamicProgramming;
 import java.util.Scanner;
 
 /**
- * @author akhash
+ * @author Akhash Ramamurthy
  *
  */
 public class Fibbonaci {
 	private static int RECURSION_COUNTER = 0;
+	private static int LOOP_COUNTER = 0;
 
 	public static void main(String[] args) {
 		System.out.println("Enter a number :");
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
 		scanner.close();
-		System.out.println("Fib - Recursive - "+fibonacciRecusion(n)+" --> RecursionCount - "+RECURSION_COUNTER);
-//		printFibonacci(n);
-
+		printFibonacciUsing3Variables(n);
+		printFibStartToEnd(n);
 	}
 
+	//Classic recusive approach. Doesnt work for n >= 100
 	public static long getFib_Recursive(int n){
 		RECURSION_COUNTER++;
 		if(n <= 0)
@@ -30,24 +31,35 @@ public class Fibbonaci {
 			return 1;
 		return getFib_Recursive(n-1) + getFib_Recursive(n-2);
 	}
+	
+	//Bottom Up DP approach
+	public static void printFibStartToEnd(int n){
+		if(n <= 0)
+			System.out.println(0);
+		else{
+			System.out.println(0);
+			System.out.println(1);
+		}
+		long res[] = new long[n+1];
+		res[1] = 0;
+		res[2] = 1;
+		for(int i = 3; i <= n; i++){
+			LOOP_COUNTER++;
+			res[i] = res[i-1] + res[i-2];
+			System.out.println(res[i]);
+		}
+		System.out.println("Loop Counter - "+LOOP_COUNTER);
+	}
 
+	//Using variables approach
 	static long n1=0,n2=1,n3=0; 
-	static void printFibonacci(int count){    
+	static void printFibonacciUsing3Variables(int count){    
 		if(count>0){    
 			n3 = n1 + n2;    
 			n1 = n2;    
 			n2 = n3;    
 			System.out.print(" "+n3);   
-			printFibonacci(count-1);    
+			printFibonacciUsing3Variables(count-1);    
 		}    
-	}    
-
-	public static int fibonacciRecusion(int number) {
-		if (number == 1 || number == 2) {
-			return 1;
-		}
-		RECURSION_COUNTER++;
-		return fibonacciRecusion(number - 1) + fibonacciRecusion(number - 2); // tail recursion
-	}
-
+	}   
 }
