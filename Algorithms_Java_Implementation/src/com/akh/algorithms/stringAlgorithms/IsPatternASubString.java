b1ee2f03df2e624588cb67174333b9fb44d6b483
ramamurthy <akhashr@gmail.com>
2017-09-10 11:49:59 -0700

    Fixed KMP algorithm implementation bug. Previously was doing O(N*M) with incorrect impl. Now fixed. Does solve in O(N+M)

--- Algorithms_Java_Implementation/src/com/akh/algorithms/stringAlgorithms/IsPatternASubString.java
+++ Algorithms_Java_Implementation/src/com/akh/algorithms/stringAlgorithms/IsPatternASubString.java
@@ -2,6 +2,8 @@ package com.akh.algorithms.stringAlgorithms;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 /**
@@ -11,123 +13,150 @@ import org.junit.Test;
  */
 
 public class IsPatternASubString {
+	
+	/********************************* Brute Force - O(N*M) ******************************************************/
+		public static boolean containsSubString_bruteForce(String string, String pattern){
+			int iLength = string.length();
+			int pLength = pattern.length();
+			int matchStartIndex = -1;
 
-	public static boolean isSubString(String s, String p){
-
+			for(int i=0; i<iLength-pLength; i++){
+				int iIndex = i, pIndex = 0;
+				while(pIndex < pLength){
+					if(string.charAt(iIndex) == pattern.charAt(pIndex)){
+						if(pIndex == 0)
+							matchStartIndex = iIndex;
+						iIndex++;
+						pIndex++;
+					}else{
+						if(matchStartIndex == -1){
+							iIndex++;
+						}else{
+							matchStartIndex = -1;
+							break;
+						}
+						pIndex = 0;
+					}
+				}
+			}
+			return matchStartIndex != -1;
+		}
+		
+	/**************************************** Knuth Morris Pratt (KMP) ***************************************************
+	 * Overall - O(N+M); N->text.len; M->pattern.len
+	 * Step 1 - Construct SuffixPrefixArray O(M)
+	 */
+	public static int[] contructSuffixPrefixArray(String p){
+		int index=0;
+		int[] lps = new int[p.length()];
+		
+		for(int j=1; j < p.length();) //j shd not be automatically incremented
+		{
+			if(p.charAt(index) == p.charAt(j))
+			{
+				lps[j] = index+1;
+				index++;
+				j++;
+			}else
+			{
+				if(index > 0){
+					index = lps[index-1];
+				}else{
+					lps[j] = 0;
+					j++;
+				}
+			}
+		}
+		System.out.println("PrefixSuffixArray : "+Arrays.toString(lps));
+		return lps;
+	}
+	
+	/**
+	* Step 2 - Using SuffixPrefixArray compare text and pattern
+	* O(N)
+	*/
+	public static boolean isSubString_KMP_Impl(String s, String p){
 		int sIndex = 0;
 		int pIndex = 0;
-		boolean matchStarted = false;
-
-		while(sIndex < s.length()-1){
-
+		int[] suffixPrefixArr = contructSuffixPrefixArray(p);
+			
+		while(sIndex < s.length() && pIndex < p.length()){
 			if(s.charAt(sIndex) == p.charAt(pIndex)){
-				//Are in the middle of matching process and just getting started
-				matchStarted = (pIndex == 0) ? true : matchStarted;
-
-				//Increment both index pointers
-				sIndex++;
-				//If last char of pattern match exit returning true
-				if(pIndex == p.length()-1)
+				if(pIndex == p.length()-1){
+					//Found pattern
 					return true;
-				pIndex++;
-			}else{
-				//Check if you are broken in the mid way while a matching started
-				//If not in stop mid way then just incr sIndex and continue
-				if(!matchStarted)
+				}else{
 					sIndex++;
-				else{
-					String suffixPrefix = isSuffixIsAlsoAPrefix(p.substring(0, pIndex));
-					if(suffixPrefix != null){
-						pIndex = suffixPrefix.length();
-					}else{
-						//Reset match found flag and pIndex - start from O index of pattern
-						pIndex = 0;
-						matchStarted = false;
-					}
+					pIndex++;
+				}
+			}else{
+				if(pIndex != 0){
+					pIndex = suffixPrefixArr[pIndex-1];
+				}else{
+					pIndex = 0;
+					sIndex++;
 				}
 			}
 		}
 		return false;
 	}
-
-	public static String isSuffixIsAlsoAPrefix(String input){
-
-		if(input == null || input.length() == 1)
-			return null;
-
-		int mid = input.length()/2;
-		int left = mid-1;
-		int right = input.length()-1;
-		int matchCharCount = -1;
-
-		while(left >= 0){
-			if(input.charAt(left) == input.charAt(right)){
-				matchCharCount = (matchCharCount == -1) ? left+1 : matchCharCount;
-				left--;
-				right--;
-			}else{
-				matchCharCount = -1;
-				left--;
-			}
-		}
-		return (matchCharCount != -1) ? input.substring(0, matchCharCount) : null;
-	}
-
+	
+	
+	/***************************************** Test Cases ******************************************************/
 	@Test
 	public void testValidInput_1(){
 		String s = "ABCBABCBABDO";
 		String p = "BABD";
-		assertTrue(isSubString(s, p));
+		assertTrue(isSubString_KMP_Impl(s, p));
 	}
 
 	@Test
 	public void testValidInput_2(){
 		String s = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
 		String p = "AAAAA";
-		assertTrue(isSubString(s, p));
+		assertTrue(isSubString_KMP_Impl(s, p));
 	}
 
 	@Test
 	public void testFailure(){
 		String s = "BBBBBBBBBBBBBBBB";
 		String p = "AAAA";
-		assertFalse(isSubString(s, p));
+		assertFalse(isSubString_KMP_Impl(s, p));
 	}
 
 	@Test
 	public void testValidInput_3(){
 		String s = "BBBBBBBBBBBBBBBBCDEFBBBBBCDEFGHBBB";
 		String p = "BBBCDEFG";
-		assertTrue(isSubString(s, p));
+		assertTrue(isSubString_KMP_Impl(s, p));
 	}
 	
+	@Test
+	public void testValidInput_4(){
+		String s = "abcxabcdabcdabcy";
+		String p = "abcdabcy";
+		assertTrue(isSubString_KMP_Impl(s, p));
+	}
 	
-	//Brute Force - O(N*M)
-	public static boolean containsSubString_bruteForce(String string, String pattern){
-		int iLength = string.length();
-		int pLength = pattern.length();
-		int matchStartIndex = -1;
-		
-		for(int i=0; i<iLength-pLength; i++){
-			int iIndex = i, pIndex = 0;
-			while(pIndex < pLength){
-				if(string.charAt(iIndex) == pattern.charAt(pIndex)){
-					if(pIndex == 0)
-						matchStartIndex = iIndex;
-					iIndex++;
-					pIndex++;
-				}else{
-					if(matchStartIndex == -1){
-						iIndex++;
-					}else{
-						matchStartIndex = -1;
-						break;
-					}
-					pIndex = 0;
-				}
-			}
-		}
-		return matchStartIndex != -1;
+	@Test
+	public void testValidInput_5(){
+		String s = "ABCXABCDABXABCDABCDABCY";
+		String p = "ABCDABC";
+		assertTrue(isSubString_KMP_Impl(s, p));
+	}
+	
+	@Test
+	public void testValidInput_6(){
+		String s = "ABCXABCDABXABCDABCDABCY";
+		String p = "ABCDABCH";
+		assertFalse(isSubString_KMP_Impl(s, p));
 	}
 
+	@Test
+	public void testContructSuffixPrefixArray_1(){
+		String pattern = "ACACABACACABACACAC";
+		int[] expected = new int[]{0,0,1,2,3,0,1,2,3,4,5,6,7,8,9,10,11,4};
+		assertArrayEquals(expected, contructSuffixPrefixArray(pattern));
+	}
+	
 }
