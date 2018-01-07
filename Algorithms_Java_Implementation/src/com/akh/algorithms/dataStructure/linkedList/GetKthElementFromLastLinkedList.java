9727a5ae07e2704abfbdfeca1b99d98f78a94436
Akhash Ramamurthy <akhashr@gmail.com>
2018-01-07 00:15:33 -0800

    Added new double pointer implementation to find Kth node from the last in a Singly LinkedList

--- Algorithms_Java_Implementation/src/com/akh/algorithms/dataStructure/linkedList/GetKthElementFromLastLinkedList.java
+++ Algorithms_Java_Implementation/src/com/akh/algorithms/dataStructure/linkedList/GetKthElementFromLastLinkedList.java
@@ -15,17 +15,31 @@ public class GetKthElementFromLastLinkedList {
 			n = n.next;
 		}
 		
-		for(int i=0; i<k; i++){
+		for(int i=1; i<k; i++){
 			stack.pop();
 		}
 		
 		return stack.pop();
 	}
+	
+	public static Node getKthElement_UsingDoublePointer(Node head, int k){
+		Node slow = head;
+		Node fast = head;
+		for(int i=1; i<=k; i++){
+			fast = fast.next;
+		}
+		while(fast != null){
+			slow = slow.next;
+			fast = fast.next;
+		}
+		return slow;
+	}
 
 	
 	public static void main(String[] args){
 		SinglyLinkedList singlyLinkedList = MockDataGenerator.generateSingleLinkedList(10);
-		System.out.println("Kth to the Last Element - "+getKthElement(singlyLinkedList.head, 9).data);
+		System.out.println("Kth to the Last Element - "+getKthElement(singlyLinkedList.head, 4).data);
+		System.out.println("Kth to the Last Element Double Pointer- "+getKthElement_UsingDoublePointer(singlyLinkedList.head, 4).data);
 
 	}
 }
