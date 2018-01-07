9ec85b48b25561aa51e202c3b12acc6dd55a0dae
Akhash Ramamurthy <akhashr@gmail.com>
2018-01-07 00:15:33 -0800

    Added new double pointer implementation to find Kth node from the last in a Singly LinkedList

--- Algorithms_Java_Implementation/src/com/akh/algorithms/dataStructure/linkedList/MockDataGenerator.java
+++ Algorithms_Java_Implementation/src/com/akh/algorithms/dataStructure/linkedList/MockDataGenerator.java
@@ -7,7 +7,7 @@ public class MockDataGenerator {
 	
 	public static SinglyLinkedList generateSingleLinkedList(int size){
 		SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
-		for(int i=0; i<size; i++){
+		for(int i=1; i<=size; i++){
 			singlyLinkedList.add(i);
 		}
 		return singlyLinkedList;
