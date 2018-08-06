7b8db853ab8bc66da623d06c69c4c9702fef371c
akhr <akhashr@gmail.com>
2018-08-05 21:41:21 -0700

    Fixed implementation to check if an array is in increasing order and return true if no item or at most one item needs be deleted. If more than one required then return false.

--- Algorithms_Java_Implementation/src/com/akh/algorithms/dataStructure/array/CheckIfArrayIsIncreasingSquence.java
+++ Algorithms_Java_Implementation/src/com/akh/algorithms/dataStructure/array/CheckIfArrayIsIncreasingSquence.java
@@ -1,12 +1,19 @@
 package com.akh.algorithms.dataStructure.array;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.akh.algorithms.dataStructure.array.LongestIncreasingSequence.ALGO_TYPE;
+
 /**
  *  @fileName: CheckIfArrayIsIncreasingSquence.java
  *  @author: Akhash Ramamurthy
  *  Created on: Jun 13, 2017
  *  
  *  
- *  Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence by removing no more than one element from the array.
+ *  Given a sequence of integers as an array, determine whether it is possible 
+ *  to obtain a strictly increasing sequence by removing no more than one element from the array.
 
 Example
 
@@ -23,64 +30,62 @@ Alternately, you can remove 2 to get the strictly increasing sequence [1, 3].
 */
 
 public class CheckIfArrayIsIncreasingSquence {
-
-	public static void main(String[] args) {
-		int[] in1 = new int[]{1, 3, 2, 1};
-		int[] in2 = new int[]{1, 3, 2};
-		int[] in3 = new int[]{1, 2, 1, 2};
-		int[] in4 = new int[]{1, 2, 3, 4, 3, 6};
-		int[] in5 = new int[]{3, 5, 67, 98, 3};
-		int[] in6 = new int[]{192, -90, -30, 2, 5, 6, 10, 25, 30};
+	
+	public boolean almostIncreasingSequence(int[] arr){
+		int prev = arr[0];
+		int deleteCount = 0;
 		
-		System.out.println("Is in Incr Sqeuence - "+almostIncreasingSequence(in5));
-
-	}
-
-	static boolean almostIncreasingSequence(int[] sequence) {
-		int length = sequence.length;
-		int seqBrokenCount = 0;
-		
-		for(int i=0; i<length-1; i++){
-		
-			if(sequence[i] >= sequence[i+1]){
-				if(seqBrokenCount >= 1){
-					//already broken exit
-					return false;
-				}else{
-					seqBrokenCount++;
-					
-					if(i == 0) //If first element continue considering the 1st element is removed
-						continue;
-					
-					if(i == length-2) //last element. It can be removed
-						return true;
-					
-					if(i-1 >= 0 && isRampSpike(i, sequence)){
-						if(sequence[i-1] < sequence[i+1]){
-							continue;
-						}else{
-                            return false;
-                        }
-					} else if(i+2 <= length-1 && isRampDip(i, sequence)){
-						if(sequence[i] < sequence[i+2]){
-							continue;
-						}else{
-                            return false;
-                        }
-					} else {
-						return false;
-					}
-				}
+		for(int i=1; i< arr.length; i++){
+			if(arr[i] > prev){
+				prev = arr[i];
+			}else if(deleteCount == 0){
+				deleteCount++;
+				prev = Math.min(prev, arr[i]);
+			}
+			else{
+				return false;
 			}
 		}
-		return seqBrokenCount >= 1;
+		return true;
 	}
 	
-	static boolean isRampSpike(int i, int[] seq){
-	    return (seq[i-1] < seq[i]) && (seq[i+1] < seq[i]) && (seq[i-1] < seq[i+1]);
-	} 
-
-	static boolean isRampDip(int i, int[] seq){
-		return (seq[i] > seq[i+1]) && (seq[i+1] < seq[i+2]) && (seq[i] < seq[i+2]);
+	@Test
+	public void test_1() {
+		CheckIfArrayIsIncreasingSquence incrSequence = new CheckIfArrayIsIncreasingSquence();
+		int[] arr = new int[]{1, 3, 2, 1};
+		System.out.println("Is Increasing Sequence - "+incrSequence.almostIncreasingSequence(arr));
+		assertEquals(false, incrSequence.almostIncreasingSequence(arr));
 	}
+	
+	@Test
+	public void test_2() {
+		CheckIfArrayIsIncreasingSquence incrSequence = new CheckIfArrayIsIncreasingSquence();
+		int[] arr = new int[]{1, 3, 2};
+		System.out.println("Is Increasing Sequence - "+incrSequence.almostIncreasingSequence(arr));
+		assertEquals(true, incrSequence.almostIncreasingSequence(arr));
+	}
+	
+	@Test
+	public void test_3() {
+		CheckIfArrayIsIncreasingSquence incrSequence = new CheckIfArrayIsIncreasingSquence();
+		int[] arr = new int[]{3, 5, 67, 98, 3};
+		System.out.println("Is Increasing Sequence - "+incrSequence.almostIncreasingSequence(arr));
+		assertEquals(true, incrSequence.almostIncreasingSequence(arr));
+	}
+	
+	@Test
+	public void test_4() {
+		CheckIfArrayIsIncreasingSquence incrSequence = new CheckIfArrayIsIncreasingSquence();
+		int[] arr = new int[]{192, -90, -30, 2, 5, 6, 10, 25, 30};
+		System.out.println("Is Increasing Sequence - "+incrSequence.almostIncreasingSequence(arr));
+		assertEquals(true, incrSequence.almostIncreasingSequence(arr));
+	}
+	
+	@Test
+	public void test_5() {
+		CheckIfArrayIsIncreasingSquence incrSequence = new CheckIfArrayIsIncreasingSquence();
+		int[] arr = new int[]{1,2,5,9,6,7,4};
+		System.out.println("Is Increasing Sequence - "+incrSequence.almostIncreasingSequence(arr));
+		assertEquals(false, incrSequence.almostIncreasingSequence(arr));
+	}	
 }
