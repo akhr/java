435f71f765fd71f2a6785ea80799637ef153cba6
Akhash Ramamurthy <akhashr@gmail.com>
2018-08-08 11:25:10 -0700

    Added question.

--- Algorithms_Java_Implementation/src/com/akh/algorithms/dataStructure/tree/bst/CheckBinaryTreeIsSymmetric.java
+++ Algorithms_Java_Implementation/src/com/akh/algorithms/dataStructure/tree/bst/CheckBinaryTreeIsSymmetric.java
@@ -11,6 +11,23 @@ import org.junit.Test;
  *  @fileName: CheckBinaryTreeIsSymmetric.java
  *  @author: Akhash Ramamurthy
  *  Created on: Aug 8, 2018
+ *  Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following [1,2,2,null,3,null,3] is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+Note:
+Bonus points if you could solve it both recursively and iteratively.
  */
 
 public class CheckBinaryTreeIsSymmetric {
