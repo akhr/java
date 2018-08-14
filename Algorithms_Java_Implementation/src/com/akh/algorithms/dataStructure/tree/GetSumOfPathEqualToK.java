344475e9605d111f9368f4d58e5cdbbda7f738d3
Akhash Ramamurthy <akhashr@gmail.com>
2018-08-14 10:26:03 -0700

    Added recursion implementation to find path sum = k in tree. Removed stack impl since it was faulty. Recusion is O(N)-Every node gets visited once.

--- Algorithms_Java_Implementation/src/com/akh/algorithms/dataStructure/tree/GetSumOfPathEqualToK.java
+++ Algorithms_Java_Implementation/src/com/akh/algorithms/dataStructure/tree/GetSumOfPathEqualToK.java
@@ -6,8 +6,14 @@
 
 package com.akh.algorithms.dataStructure.tree;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.akh.algorithms.dataStructure.tree.bst.BinaryTreeNode;
@@ -24,79 +30,42 @@ import com.akh.algorithms.dataStructure.tree.bst.BinaryTreeNode;
  */
 public class GetSumOfPathEqualToK {
 	
-	Stack<BinNode> stack;
-	int sum = 0;
-	
-	public void getSumOfPathEqualToK(BinNode root, int k){
-		stack = new Stack<>();
-		findPath(root, k);
-		while(!stack.isEmpty()){
-			System.out.println(stack.pop().getName());
-		}
+	public List<BinNode> getPathRecursion(BinNode root, int k){
+		List<BinNode> result = new ArrayList<>();
+		findPath(root, k, result, 0);
+		return result;
 	}
-	
-	public void findPath(BinNode node, int k){
+	public int findPath(BinNode node, int k, List<BinNode> result, int sumSoFar) {
 		if(node == null)
-			return;
+			return 0;
+		System.out.println("Visiting Node - "+node.getName()+"  val - "+node.getData());
 		
-		stack.push(node);
-		sum = sum + node.getData();
-		
-		if(sum == k){
-			return;
+		int newSum = sumSoFar + node.getData();
+
+		if(newSum == k) {
+			result.add(node);
+			return newSum;
 		}
-		
-		if(sum < k){
-			findPath((BinNode)node.getLeft(), k);
-			findPath((BinNode)node.getRight(), k);
-		}else if(sum > k){
-			stack.pop();
-			sum = sum - node.getData();
+
+		if(newSum > k) {
+			return sumSoFar;
 		}
-		return;
+
+		if(newSum < k) {
+			result.add(node);
+			int leftPathSum = findPath((BinNode)node.getLeft(), k, result, newSum);
+			if(leftPathSum == k) {
+				return leftPathSum;
+			}
+			int rightPathSum = findPath((BinNode)node.getRight(), k, result, newSum);
+			if(rightPathSum == k) {
+				return rightPathSum;
+			}
+			result.remove(node);
+		}
+		return sumSoFar;
 	}
-	
-	@Test
-	public void testFindPath(){
-		BinNode a = new BinNode(1, "a");
-		BinNode b = new BinNode(3, "b");
-		BinNode c = new BinNode(2, "c");
-		BinNode d = new BinNode(5, "d");
-		BinNode e = new BinNode(2, "e");
-		BinNode f = new BinNode(5, "f");
-		BinNode g = new BinNode(4, "g");
-		BinNode h = new BinNode(12, "h");
-		BinNode i = new BinNode(6, "i");
-		BinNode j = new BinNode(75, "j");
-		BinNode k = new BinNode(0, "k");
-		BinNode l = new BinNode(14, "l");
-		BinNode m = new BinNode(7, "m");
-		BinNode n = new BinNode(9, "n");
-		
-		a.setLeft(b);
-		a.setRight(c);
-		
-		b.setLeft(d);
-		b.setRight(e);
-		
-		c.setLeft(f);
-		c.setRight(g);
-		
-		d.setLeft(h);
-		d.setRight(i);
-		
-		e.setLeft(j);
-		e.setRight(k);
-		
-		f.setLeft(l);
-		f.setRight(m);
-		
-		g.setLeft(n);
-		
-		GetSumOfPathEqualToK pathFinder = new GetSumOfPathEqualToK();
-		pathFinder.getSumOfPathEqualToK(a, 22);
-	}
-	
+
 	public class BinNode extends BinaryTreeNode<Integer>{
 		String name;
 		public BinNode(Integer data, String name) {
@@ -109,7 +78,132 @@ public class GetSumOfPathEqualToK {
 		public void setName(String name) {
 			this.name = name;
 		}
+
+	}
+
+	private BinNode getTree() {
+		BinNode a = new BinNode(1, "a");
+		BinNode b = new BinNode(3, "b");
+		BinNode c = new BinNode(2, "c");
+		BinNode d = new BinNode(5, "d");
+		BinNode e = new BinNode(20, "e");
+		BinNode f = new BinNode(5, "f");
+		BinNode g = new BinNode(4, "g");
+		BinNode h = new BinNode(16, "h");
+		BinNode i = new BinNode(6, "i");
+		BinNode j = new BinNode(75, "j");
+		BinNode k = new BinNode(0, "k");
+		BinNode l = new BinNode(14, "l");
+		BinNode m = new BinNode(7, "m");
+		BinNode n = new BinNode(9, "n");
+
+		a.setLeft(b);
+		a.setRight(c);
+
+		b.setLeft(d);
+		b.setRight(e);
+
+		c.setLeft(f);
+		c.setRight(g);
+
+		d.setLeft(h);
+		d.setRight(i);
+
+		e.setLeft(j);
+		e.setRight(k);
+
+		f.setLeft(l);
+		f.setRight(m);
+
+		g.setLeft(n);
+
+		return a;
+	}
+
+
+	@Test
+	public void testFindPath_Recursion_1(){
+		System.out.println("Recusion Impl....");
+		BinNode root = getTree();
+		String[] expecteds = {"a","c","f","l"};
+		GetSumOfPathEqualToK pathFinder = new GetSumOfPathEqualToK();
+		List<BinNode> result = pathFinder.getPathRecursion(root, 22);
+		String[] actuals = new String[result.size()];
+		for(int i =0; i<result.size(); i++){
+			System.out.println(result.get(i).getName()+"  val - "+result.get(i).getData());
+			actuals[i] = result.get(i).getName();
+		}
+		assertEquals("Compare Arrays Length",expecteds.length, actuals.length);
+		assertArrayEquals("Compare Arrays", expecteds, actuals);
+	}
+	
+	@Test
+	public void testFindPath_Recursion_2(){
+		System.out.println("Recusion Impl....");
+		BinNode root = getTree();
+		String[] expecteds = {};
+		GetSumOfPathEqualToK pathFinder = new GetSumOfPathEqualToK();
+		List<BinNode> result = pathFinder.getPathRecursion(root, 6);
+		String[] actuals = new String[result.size()];
+		for(int i=0; i<result.size(); i++){
+			System.out.println(result.get(i).getName()+"  val - "+result.get(i).getData());
+			actuals[i] = result.get(i).getName();
+		}
+		assertEquals("Compare Arrays Length",expecteds.length, actuals.length);
+		assertArrayEquals("Compare Arrays", expecteds, actuals);
+	}
+	
+	@Test
+	public void testFindPath_Recursion_No_Branch_b(){
+		System.out.println("Recusion Impl....");
+
+		BinNode a = new BinNode(1, "a");
+		BinNode b = new BinNode(3, "b");
+		BinNode c = new BinNode(2, "c");
+		BinNode d = new BinNode(5, "d");
+		BinNode e = new BinNode(20, "e");
+		BinNode f = new BinNode(5, "f");
+		BinNode g = new BinNode(4, "g");
+		BinNode h = new BinNode(16, "h");
+		BinNode i = new BinNode(6, "i");
+		BinNode j = new BinNode(75, "j");
+		BinNode k = new BinNode(0, "k");
+		BinNode l = new BinNode(14, "l");
+		BinNode m = new BinNode(7, "m");
+		BinNode n = new BinNode(9, "n");
+
+		a.setLeft(b);
+		a.setRight(c);
+
+		//b.setLeft(d);
+		b.setRight(e);
+
+		c.setLeft(f);
+		c.setRight(g);
+
+		//d.setLeft(h);
+		//d.setRight(i);
+
+		e.setLeft(j);
+		e.setRight(k);
+
+		f.setLeft(l);
+		f.setRight(m);
+
+		g.setLeft(n);
+
 		
+		String[] expecteds = {"a", "c", "f", "m"};
+		GetSumOfPathEqualToK pathFinder = new GetSumOfPathEqualToK();
+		List<BinNode> result = pathFinder.getPathRecursion(a, 15);
+		String[] actuals = new String[result.size()];
+		
+		for(int i1 = 0; i1<result.size(); i1++){
+			System.out.println(result.get(i1).getName()+"  val - "+result.get(i1).getData());
+			actuals[i1] = result.get(i1).getName();
+		}
+		assertEquals("Compare Arrays Length",expecteds.length, actuals.length);
+		assertArrayEquals("Compare Arrays", expecteds, actuals);
 	}
 
 }
