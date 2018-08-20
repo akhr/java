68e7fc47950dc45489a85a8ce36751691a3676a8
Akhash Ramamurthy <akhashr@gmail.com>
2018-08-20 10:03:12 -0700

    Added 1 stack implementation for PostOrder. Not my solution. From StackOverflow. Needs further understanding.

--- Algorithms_Java_Implementation/src/com/akh/algorithms/dataStructure/tree/PostOrder_Using_Stack.java
+++ Algorithms_Java_Implementation/src/com/akh/algorithms/dataStructure/tree/PostOrder_Using_Stack.java
@@ -1,10 +1,9 @@
 package com.akh.algorithms.dataStructure.tree;
 
+import java.util.LinkedList;
 import java.util.Stack;
 
 import org.junit.Test;
-
-import com.akh.algorithms.dataStructure.tree.PreOrder_Using_Stack.TreeNode;
 
 /**
  *  @fileName: PostOrder_Using_Stack.java
@@ -19,19 +18,19 @@ public class PostOrder_Using_Stack {
 		TreeNode right;
 		TreeNode(int x) { val = x; }
 	}
-	
-	
+
+
 	//=======================================================================================
-	
+
 	//root- left - right
 	public void postOrder_iterative_2_stack(TreeNode root) {
 		if(root == null)
 			return;
-		
+
 		Stack<TreeNode> s1 = new Stack<>();
 		Stack<TreeNode> s2 = new Stack<>();
 		s1.push(root);
-		
+
 		while(!s1.isEmpty()) {
 			TreeNode s1Poped = s1.pop();
 			s2.push(s1Poped);
@@ -40,41 +39,58 @@ public class PostOrder_Using_Stack {
 			if(s1Poped.right != null)
 				s1.push(s1Poped.right);
 		}
-		
+
 		while(!s2.isEmpty()) {
 			System.out.println("Visiting Node (I 2 stack) - "+s2.pop().val);
 		}
-		
+
 	}
+
 	
-	public void postOrder_iterative_1_stack(TreeNode root) {
-		if(root == null)
+	//Needs understanding
+	public void postOrder_iterative_1_stack(TreeNode head) {
+		if (head == null) {
 			return;
-		
-		Stack<TreeNode> stack = new Stack<>();
-		TreeNode curr = root;
-		
-		while(curr != null && !stack.isEmpty()) {
-			
 		}
-		
-		System.out.println("Visiting Node (I 2 stack) - "+curr.val);		
+		LinkedList<TreeNode> stack = new LinkedList<TreeNode>();
+		stack.push(head);
+		TreeNode lastPoped = head;
+
+		while (!stack.isEmpty()) {
+			TreeNode next = stack.peek();
+
+			boolean finishedSubtrees = (next.right == lastPoped || next.left == lastPoped);
+			boolean isLeaf = (next.left == null && next.right == null);
+			if (finishedSubtrees || isLeaf) {
+				stack.pop();
+				System.out.println(next.val);
+				lastPoped = next;
+			}
+			else {
+				if (next.right != null) {
+					stack.push(next.right);
+				}
+				if (next.left != null) {
+					stack.push(next.left);
+				}
+			}
+		}
 	}
-	
+
 	public void postOrder_recursive(TreeNode root) {
 		if(root == null)
 			return;
-	
+
 		postOrder_recursive(root.left);
 		postOrder_recursive(root.right);
 		System.out.println("Visiting Node (R) - "+root.val);
 	}
 	//=======================================================================================
-	
-	
-	
-	
-	
+
+
+
+
+
 	private TreeNode constructTree(int[] data, int index) {
 		if(index > data.length-1)
 			return null;
@@ -88,15 +104,15 @@ public class PostOrder_Using_Stack {
 		node.right = constructTree(data, getRightChildIndex(index));
 		return node;
 	}
-	
+
 	private int getLeftChildIndex(int parentIndex) {
 		return (parentIndex*2)+1;
 	}
-	
+
 	private int getRightChildIndex(int parentIndex) {
 		return (parentIndex*2)+2;
 	}
-	
+
 	@Test
 	public void test_recursive() {
 		PostOrder_Using_Stack postOrder = new PostOrder_Using_Stack();
@@ -105,13 +121,22 @@ public class PostOrder_Using_Stack {
 		System.out.println("Recursive");
 		postOrder.postOrder_recursive(root);
 	}
+
+	@Test
+	public void test_iterative_1_stack() {
+		PostOrder_Using_Stack postOrder = new PostOrder_Using_Stack();
+		int[] arr = {1,2,3,4,5,-1,7,-1,-1,6,-1,-1,-1,-1,8};
+		TreeNode root = postOrder.constructTree(arr, 0);
+		System.out.println("Iterative 1 Stack");
+		postOrder.postOrder_iterative_1_stack(root);
+	}
 	
 	@Test
 	public void test_iterative_2_stack() {
 		PostOrder_Using_Stack postOrder = new PostOrder_Using_Stack();
 		int[] arr = {1,2,3,4,5,-1,-1,-1,-1,6};
 		TreeNode root = postOrder.constructTree(arr, 0);
-		System.out.println("Iterative");
+		System.out.println("Iterative 2 Stack");
 		postOrder.postOrder_iterative_2_stack(root);
 	}
 }
