d6bbad41aa86b3b7993e892c7956ab5996eaf86c
akhr <akhashr@gmail.com>
2018-08-26 17:34:24 -0700

    Implementation for setting matrix zeros using no extra memory. Space O(1). Time O(m*n)

--- Algorithms_Java_Implementation/src/com/akh/algorithms/dataStructure/array/matrix/SetMatrixZeros.java
+++ Algorithms_Java_Implementation/src/com/akh/algorithms/dataStructure/array/matrix/SetMatrixZeros.java
@@ -47,7 +47,7 @@ import org.junit.Test;
 
 public class SetMatrixZeros {
 
-	public void setZeroes(int[][] matrix) {
+	public void setZeroes_1(int[][] matrix) {
 		int rowCount = matrix.length;
 		int colCount = matrix[0].length;
 
@@ -89,6 +89,44 @@ public class SetMatrixZeros {
 			}
 		}
 	}
+	
+	public void setZeroes_2(int[][] matrix) {
+		int rowCount = matrix.length;
+		int colCount = matrix[0].length;
+		int newVal = Integer.MIN_VALUE;
+
+		for(int i=0; i<rowCount; i++) {
+			for(int j=0; j<colCount; j++) {
+				if(matrix[i][j] == 0) {
+					replaceRowsAndCols(matrix, i, j, newVal);
+				}
+			}
+		}
+		
+		for(int i=0; i<rowCount; i++) {
+			for(int j=0; j<colCount; j++) {
+				if(matrix[i][j] == newVal) {
+					matrix[i][j] = 0;
+				}
+			}
+		}
+	}
+	
+	public void replaceRowsAndCols(int[][] matrix, int rowId, int colId, int newVal){
+		int j=0;
+		while(j<matrix[0].length){
+			if(matrix[rowId][j] != 0)
+				matrix[rowId][j] = newVal;
+			j++;
+		}
+		
+		int i=0;
+		while(i<matrix.length){
+			if(matrix[i][colId] != 0)
+				matrix[i][colId] = newVal;
+			i++;
+		}
+	}
 
 	@Test
 	public void test_1() {
@@ -97,7 +135,7 @@ public class SetMatrixZeros {
 		matrix[1] = new int[]{1,0,1};
 		matrix[2] = new int[]{1,1,1};
 
-		setZeroes(matrix);
+		setZeroes_1(matrix);
 		
 		System.out.println("Test 1");
 		for(int i=0; i<3; i++) {
@@ -115,7 +153,7 @@ public class SetMatrixZeros {
 		matrix[1] = new int[]{3,4,5,2};
 		matrix[2] = new int[]{1,3,1,5};
 
-		setZeroes(matrix);
+		setZeroes_1(matrix);
 
 		System.out.println("\nTest 2");
 		for(int i=0; i<3; i++) {
@@ -126,4 +164,39 @@ public class SetMatrixZeros {
 		}
 	}
 	
+	@Test
+	public void test_3() {
+		int[][] matrix = new int[3][3];
+		matrix[0] = new int[]{1,1,1};
+		matrix[1] = new int[]{1,0,1};
+		matrix[2] = new int[]{1,1,1};
+
+		setZeroes_2(matrix);
+		
+		System.out.println("\nTest 3");
+		for(int i=0; i<3; i++) {
+			System.out.print("\n");
+			for(int j=0; j<3; j++) {
+				System.out.print("\t"+matrix[i][j]);
+			}
+		}
+	}
+	
+	@Test
+	public void test_4() {
+		int[][] matrix = new int[3][4];
+		matrix[0] = new int[]{0,1,2,0};
+		matrix[1] = new int[]{3,4,5,2};
+		matrix[2] = new int[]{1,3,1,5};
+
+		setZeroes_2(matrix);
+
+		System.out.println("\nTest 4");
+		for(int i=0; i<3; i++) {
+			System.out.print("\n");
+			for(int j=0; j<4; j++) {
+				System.out.print("\t"+matrix[i][j]);
+			}
+		}
+	}
 }
