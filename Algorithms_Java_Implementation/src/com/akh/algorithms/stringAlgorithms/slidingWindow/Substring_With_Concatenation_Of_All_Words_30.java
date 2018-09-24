1d9af341720b216ef2a165c9012213dae6768303
akhr <akhashr@gmail.com>
2018-09-24 08:49:41 -0700

    Implementation for Substring with Concatenation of All Words using Sliding Window technique.

--- Algorithms_Java_Implementation/src/com/akh/algorithms/stringAlgorithms/slidingWindow/Substring_With_Concatenation_Of_All_Words_30.java
+++ Algorithms_Java_Implementation/src/com/akh/algorithms/stringAlgorithms/slidingWindow/Substring_With_Concatenation_Of_All_Words_30.java
@@ -1,89 +1,193 @@
 package com.akh.algorithms.stringAlgorithms.slidingWindow;
 
+import static org.junit.Assert.assertArrayEquals;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.Test;
+
 /**
  *  @fileName: Substring_With_Concatenation_Of_All_Words_30.java
  *  @author: Akhash Ramamurthy
  *  Created on: Sep 21, 2018
+ *  
+ * 		You are given a string, s, and a list of words, words, that are all of the same length. Find all starting indices of substring(s) in s that is a concatenation of each word in words exactly once and without any intervening characters.
+
+		Example 1:
+
+		Input:
+		  s = "barfoothefoobarman",
+		  words = ["foo","bar"]
+		Output: [0,9]
+		Explanation: Substrings starting at index 0 and 9 are "barfoor" and "foobar" respectively.
+		The output order does not matter, returning [9,0] is fine too.
+		Example 2:
+
+		Input:
+		  s = "wordgoodstudentgoodword",
+		  words = ["word","student"]
+		Output: []
  */
 
 public class Substring_With_Concatenation_Of_All_Words_30 {
 
-	public List<Integer> findSubstring(String s, String[] words) {
+/*	public List<Integer> findSubstring(String s, String[] words) {
+
 		List<Integer> res = new ArrayList<>();
 		if(s == null | s.length()<=0 || words==null || words.length==0)
 			return res;
 
-		Map<Character, Integer> charsToFind = new HashMap<>();
 		Map<String, Integer> wordsToFind = new HashMap<>();
-		int requiredCharsCount = 0;
-
+		int wL = words[0].length(); //Because all words are of same length
 		for(String word: words) {
+			if(word.length() != wL) //All words shd be of equal length else return
+				return res;
 			wordsToFind.put(word, wordsToFind.getOrDefault(word, 0)+1);
 		}
-		
-		//Load charsTofindMap and get requriedCharsCnt
-		requiredCharsCount = loadCharsToFindMap(charsToFind, words);
-			
+
 		int left, right;
 		left = right = 0;
-		int foundCharsCount = 0;
-		int wL = words[0].length(); //All words are of same length
+		int requiredWordsCount = 0;
 
-		while(right < s.length()) {
-			char expandChar = s.charAt(right);
-			if(charsToFind.containsKey(expandChar) && charsToFind.get(expandChar) > 0) {
-				foundCharsCount++;
-				charsToFind.put(expandChar, charsToFind.getOrDefault(expandChar, 0)-1);
-				++right;
-			}else {
-				
-				//Restart Window
-				//Unwanted char || Wanted char but count more than required. .
-				left = ++right;
-				foundCharsCount = 0;
-				loadCharsToFindMap(charsToFind, words);
+		while((right+wL)<=s.length()){
+			String expandWord = s.substring(right, right+wL);
+			if(wordsToFind.containsKey(expandWord)){
+				if(wordsToFind.get(expandWord) > 0){
+					requiredWordsCount++;
+				}
+				wordsToFind.put(expandWord, wordsToFind.get(expandWord)-1);
+				right += wL;
+			}else{
+				right++;
 			}
-			
-			if(foundCharsCount == requiredCharsCount && (right-left) == requiredCharsCount) {
-				String window = s.substring(left, right);
-				if(isValidWindow(window, wordsToFind, words.length, wL))
+
+			while(left < right && requiredWordsCount == words.length){
+				String shrinkWord = s.substring(left, left+wL);
+				if(wordsToFind.containsKey(shrinkWord)){
+					if(wordsToFind.get(shrinkWord) == 0){
+						requiredWordsCount--;
+					}
+					wordsToFind.put(shrinkWord, wordsToFind.get(shrinkWord)+1);
+				}
+
+				if(s.substring(left, right).length() == (words.length*wL))
 					res.add(left);
-				
-				//Restart Window
-				left = right; 
-				foundCharsCount = 0;
-				loadCharsToFindMap(charsToFind, words);
+				left++;
 			}
 		}
 		return res;
-	}
-	
-	private boolean isValidWindow(String window, Map<String, Integer> wordsToFind, int wordsCnt, int wordSize) {
+	}*/
 
-		int i = 0;
-		int foundWordsCnt = 0;
-		while(i < window.length()) {
-			String windowWord = window.substring(i, i+wordSize);
-			if(wordsToFind.containsKey(windowWord)) foundWordsCnt++;
-			i += wordSize;
-		}
-		return foundWordsCnt == wordsCnt;
-	}
-	
-	private int loadCharsToFindMap(Map<Character, Integer> charsToFind, String[] words) {
-		int requiredCharsCount = 0;
-		for(String word: words) {
-			for(char c : word.toCharArray()) {
-				charsToFind.put(c, charsToFind.getOrDefault(c, 0)+1);
-				requiredCharsCount++;
+	public List<Integer> findSubstring(String s, String[] words){
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		if(s==null||s.length()==0||words==null||words.length==0){
+			return result;
+		} 
+
+		//frequency of words
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		for(String w: words){
+			if(map.containsKey(w)){
+				map.put(w, map.get(w)+1);
+			}else{
+				map.put(w, 1);
 			}
 		}
-		return requiredCharsCount;
+
+		int len = words[0].length();
+
+		for(int j=0; j<len; j++){
+			HashMap<String, Integer> currentMap = new HashMap<String, Integer>();
+			int start = j;//start index of start
+			int count = 0;//count totoal qualified words so far
+
+			for(int i=j; i<=s.length()-len; i=i+len){
+				String sub = s.substring(i, i+len);
+				if(map.containsKey(sub)){
+					//set frequency in current map
+					if(currentMap.containsKey(sub)){
+						currentMap.put(sub, currentMap.get(sub)+1);
+					}else{
+						currentMap.put(sub, 1);
+					}
+
+					count++;
+
+					while(currentMap.get(sub)>map.get(sub)){
+						String left = s.substring(start, start+len);
+						currentMap.put(left, currentMap.get(left)-1);
+
+						count--;
+						start = start + len;
+					}
+
+
+					if(count==words.length){
+						result.add(start); //add to result
+
+						//shift right and reset currentMap, count & start point         
+						String left = s.substring(start, start+len);
+						currentMap.put(left, currentMap.get(left)-1);
+						count--;
+						start = start + len;
+					}
+				}else{
+					currentMap.clear();
+					start = i+len;
+					count = 0;
+				}
+			}
+		}
+
+		return result;
+	}
+
+	@Test
+	public void test_1(){
+		String s = "barfoothefoobarman";
+		String[] words = {"foo","bar"};
+		int[] expected = {0,9};
+		int[] actual = findSubstring(s, words).stream().mapToInt(i->i).toArray();
+		assertArrayEquals(expected, actual);
+	}
+
+	@Test
+	public void test_2(){
+		String s = "wordgoodstudentgoodword";
+		String[] words = {"word","student"};
+		int[] expected = {};
+		int[] actual = findSubstring(s, words).stream().mapToInt(i->i).toArray();
+		assertArrayEquals(expected, actual);
+	}
+
+	@Test
+	public void test_3(){
+		String s = "lingmindraboofooowingdingbarrwingmonkeypoundcake";
+		String[] words = {"fooo","barr","wing","ding","wing"};
+		int[] expected = {13};
+		int[] actual = findSubstring(s, words).stream().mapToInt(i->i).toArray();
+		assertArrayEquals(expected, actual);
+	}
+
+	@Test
+	public void test_4(){
+		String s = "aaaaaa";
+		String[] words = {"aaa","aaa"};
+		int[] expected = {0};
+		int[] actual = findSubstring(s, words).stream().mapToInt(i->i).toArray();
+		assertArrayEquals(expected, actual);
+	}
+
+	@Test
+	public void test_5(){
+		String s = "aaaaaaaa";
+		String[] words = {"aa","aa", "aa"};
+		int[] expected = {0,1,2};
+		int[] actual = findSubstring(s, words).stream().mapToInt(i->i).toArray();
+		assertArrayEquals(expected, actual);
 	}
 
 }
