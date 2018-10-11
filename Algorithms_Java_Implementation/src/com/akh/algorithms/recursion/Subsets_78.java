69a3e0f3a8dc9081e7c904a89f11f2933cdb1a06
akhr <akhashr@gmail.com>
2018-10-11 10:55:37 -0700

    Added DFS Iterative Pre-Order solution for generating subsets for a given array.

--- Algorithms_Java_Implementation/src/com/akh/algorithms/recursion/Subsets_78.java
+++ Algorithms_Java_Implementation/src/com/akh/algorithms/recursion/Subsets_78.java
@@ -2,7 +2,10 @@ package com.akh.algorithms.recursion;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
 
 import org.junit.Test;
 
@@ -29,51 +32,100 @@ import org.junit.Test;
 		  [1,2],
 		  []
 ]
+
+
  */
 
 
 
 public class Subsets_78 {
 
-	public List<List<Integer>> subsets(int[] nums) {
-		List<List<Integer>> all = new ArrayList<>();
-		List<Integer> oneResult = new ArrayList<>();
-		dfs(nums, 0, oneResult, all);
-		return all;
+	public List<List<Integer>> subsets_DFS_Recursive(int[] nums) {
+		List<List<Integer>> result = new ArrayList<>();
+		List<Integer> input = new ArrayList<>();
+		// Start with empty input (input.size == 0) to recursion
+		// input is the key to these recursive calls
+		// Inside recursive func... 
+		// 1.You will do/add something to the input and call its children passing the new input 
+		dfs_Recursive(nums, 0, input, result);
+		return result;
 	}
 
-	private void dfs(int[] nums, int position, List<Integer> oneResult, List<List<Integer>> all) {
+	private void dfs_Recursive(int[] nums, int position, List<Integer> input, List<List<Integer>> result) {
 
 		// base case
-		// If gone past leaf node the its time to 
-		// 1. Create new subSet
-		// 2. Copy contents of oneResult to new subSet
-		// 3. return
+		// If (position ==  nums.length) it means we have no other item in nums[] to go further down
+		// Its a good time to RECORD the result of this branch to results[].. So
 		if(position == nums.length) {
-			List<Integer> newSubset = new ArrayList<>();
-			newSubset.addAll(oneResult);
-			all.add(newSubset);
+			result.add(new ArrayList<>(input));
 			return;
 		}
 
-		//Case 1 - Not including nums[position] in subset.. direct go to next recursive call
-		dfs(nums, position+1, oneResult, all);
+		// Case 1 - Do not include self(nums[postion]) in input... 
+		// So directly call next recursion call and pass input without changes (as it was received)
+		dfs_Recursive(nums, position+1, input, result);
 		//Once this step is complete, subSet would have been added to all[]
 
 		//Case 2 - Including nums[position] in subset.. add to subset and then go to next recursive call
-		oneResult.add(nums[position]);
-		dfs(nums, position+1, oneResult, all);
+		input.add(nums[position]);
+		dfs_Recursive(nums, position+1, input, result);
 		//Once this step is complete, subSet would have been added to all[]
 
 		//While returning from this TREE LEVEL give oneResult as it was received. Remove nums[position] added to oneResult 
-		oneResult.remove(oneResult.size()-1);
+		input.remove(input.size()-1);
 	}
+	
+	public List<List<Integer>> subsets_DFS_Iterative(int[] nums) {
+		if(nums == null || nums.length <= 0)
+			return new ArrayList<>();
+		
+		List<List<Integer>> res = new ArrayList<>();
 
+		Stack<Integer> levelStack = new Stack<>();
+		List<Integer> tempList = new ArrayList<>();
+		levelStack.push(0);
+		
+		int listCnt = 0;
+		
+		while(!levelStack.isEmpty()){
+			int pos = levelStack.pop();
+			if(pos == nums.length){
+				listCnt++;
+				res.add(new ArrayList<>(tempList));
+				if(!tempList.isEmpty())
+					tempList.remove(tempList.size()-1);
+				continue;
+			}
+			
+			levelStack.push(pos+1);
+			
+			tempList.add(nums[pos]);
+			levelStack.push(pos+1);
+		}
+		System.out.println("# of Lists created : "+listCnt);
+		return res;
+	}
+	
+/*	public List<List<Integer>> subsets_BFS(int[] nums) {
+		if(nums == null || nums.length <= 0)
+			return new ArrayList<>();
+		
+		List<List<Integer>> res = new ArrayList<>();
+		Queue<Integer> levelQueue = new LinkedList<>();
+	}
+*/
 
 	@Test
 	public void subSet_1() {
 		int[] nums = {1,2,3};
-		List<List<Integer>> actual = subsets(nums);
+		List<List<Integer>> actual = subsets_DFS_Recursive(nums);
+		System.out.println("Result : "+Arrays.toString(actual.toArray()));
+	}
+	
+	@Test
+	public void subSet_2() {
+		int[] nums = {1,2,3};
+		List<List<Integer>> actual = subsets_DFS_Iterative(nums);
 		System.out.println("Result : "+Arrays.toString(actual.toArray()));
 	}
 }
