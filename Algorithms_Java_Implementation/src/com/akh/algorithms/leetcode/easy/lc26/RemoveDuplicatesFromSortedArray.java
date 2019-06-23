346cf5fa3c40598735d5cf3dc177f6b5be87dcd3
akhr <akhashr@gmail.com>
2019-06-23 12:18:05 -0700

    Refactor

---
+++ Algorithms_Java_Implementation/src/com/akh/algorithms/leetcode/easy/lc26/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,60 @@
+/**
+ * @fileName: RemoveDuplicatesFromSortedArray.java
+ * @author: Akhash Ramamurthy
+ * @CreatedOn: Jun 22, 2019
+ * 
+ */
+package com.akh.algorithms.leetcode.easy.lc26;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @fileName: RemoveDuplicatesFromSortedArray.java
+ * @author: Akhash Ramamurthy
+ * @Created on: Jun 22, 2019
+ * 
+ */
+public class RemoveDuplicatesFromSortedArray {
+
+	public int removeDuplicates(int[] nums) {
+		if (nums.length <= 1) {
+			return 1;
+		}
+		int swapIndx = 1;
+		int lastNum = nums[0];
+		for (int num : nums) {
+			if (num != lastNum) {
+				lastNum = num;
+				nums[swapIndx] = num;
+				swapIndx++;
+			}
+		}
+		return swapIndx;
+	}
+
+
+	@Test
+	public void test_101() {
+		RemoveDuplicatesFromSortedArray remoDups = new RemoveDuplicatesFromSortedArray();
+		int[] in = new int[] {1,1,2};
+		int res = remoDups.removeDuplicates(in);
+		assertEquals(2, res);
+	}
+	
+	@Test
+	public void test_102() {
+		RemoveDuplicatesFromSortedArray remoDups = new RemoveDuplicatesFromSortedArray();
+		int[] in = new int[] {0,0,1,1,1,2,2,3,3,4};
+		int res = remoDups.removeDuplicates(in);
+		assertEquals(5, res);
+	}
+	
+	@Test
+	public void test_103() {
+		RemoveDuplicatesFromSortedArray remoDups = new RemoveDuplicatesFromSortedArray();
+		int[] in = new int[] {1,1,2};
+		int res = remoDups.removeDuplicates(in);
+		assertEquals(2, res);
+	}
+}
